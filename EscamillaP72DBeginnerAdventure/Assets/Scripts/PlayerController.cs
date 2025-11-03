ba34628be506a021913137a7790647dc55b49369
NachoLibre67 <736939@student.dusd.net>
2025-11-03 15:26:21 -0800

    i added the tile pallet
    added the tile pallet and began character movement thing then decorATED A BIT

--- EscamillaP72DBeginnerAdventure/Assets/Scripts/PlayerController.cs
+++ EscamillaP72DBeginnerAdventure/Assets/Scripts/PlayerController.cs
@@ -7,12 +7,14 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 position = transform.position;
+        position.x = position.x + 01f;
+        transform.position = position;
     }
 }
