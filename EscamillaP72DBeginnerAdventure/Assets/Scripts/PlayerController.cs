20709ee1937f527e93cb7a864c4d03ac4dfaf926
NachoLibre67 <736939@student.dusd.net>
2025-11-12 15:27:24 -0800

    added ntitles and did part of the movement script
    i added 45 tiles so i need one more and left the movement script incomplete fix it next time

--- EscamillaP72DBeginnerAdventure/Assets/Scripts/PlayerController.cs
+++ EscamillaP72DBeginnerAdventure/Assets/Scripts/PlayerController.cs
@@ -2,48 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[DebuggerDisplay("{" + nameof(GetDebuggerDisplay) + "(),nq}")]
 public class PlayerController : MonoBehaviour
 {
+    public InputAction MoveAction;
+
+
     // Start is called before the first frame update
     void Start()
     {
-
+        MoveAction.Enable();
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = 0.0f;
-        if (Keyboard.current.leftArrowKey.isPressed)
-        {
-            horizontal = -1.0f;
-        }
-        else if (Keyboard.current.rightArrowKey.isPressed)
-        {
-            horizontal = 1.0f;
-        }
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+        Vector2 position = (Vector2)transform.position + move * 0.1f;
+        transform.position = position;
+    }
 
-        UnityEngine.Debug.Log(horizontal);
-
-        float vertical = 0.0f;
-        if (Keyboard.current.upArrowKey.isPressed)
-        {
-            vertical = -1.0f;
-        }
-        else if (Keyboard.current.downArrowKey.isPressed)
-        {
-            vertical = 1.0f;
-        }
-        UnityEngine.Debug.Log(vertical);
-
-
-            Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal;
-        position.y = position.y + 0.1f * vertical;
-       transform.position = position;
+    private string GetDebuggerDisplay()
+    {
+        return ToString();
     }
 }
