bfeea9c61a062e0510e352485a708cd6c467c891
NachoLibre67 <736939@student.dusd.net>
2025-11-14 15:28:33 -0800

    added movement and the final tiles needed
    i added the movment script and it reads the player input actionsd correctly

--- EscamillaP72DBeginnerAdventure/Assets/Scripts/PlayerController.cs
+++ EscamillaP72DBeginnerAdventure/Assets/Scripts/PlayerController.cs
@@ -1,36 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
-using TMPro;
-using Unity.VisualScripting;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[DebuggerDisplay("{" + nameof(GetDebuggerDisplay) + "(),nq}")]
+
 public class PlayerController : MonoBehaviour
 {
+    
     public InputAction MoveAction;
-
-
     // Start is called before the first frame update
     void Start()
     {
+
         MoveAction.Enable();
     }
 
 
-    // Update is called once per frame
-    void Update()
-    {
+   // Update is called once per frame
+   void Update()
+   {
         Vector2 move = MoveAction.ReadValue<Vector2>();
         Debug.Log(move);
+    
         Vector2 position = (Vector2)transform.position + move * 0.1f;
         transform.position = position;
-    }
-
-    private string GetDebuggerDisplay()
-    {
-        return ToString();
-    }
+   }
 }
