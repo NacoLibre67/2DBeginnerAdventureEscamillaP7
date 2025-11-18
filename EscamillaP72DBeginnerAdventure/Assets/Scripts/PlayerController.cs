a1e2f540ee1d1450ef29d6400bb0ce9e870fbf37
NachoLibre67 <736939@student.dusd.net>
2025-11-18 15:27:33 -0800

    i added the health script and the grid/ title map colider*
    the grid or tile map is on a seperate one but works almost the exact same to make sure I didnt have to change tile colider  types for every tile

--- EscamillaP72DBeginnerAdventure/Assets/Scripts/PlayerController.cs
+++ EscamillaP72DBeginnerAdventure/Assets/Scripts/PlayerController.cs
@@ -6,23 +6,45 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    
+   
     public InputAction MoveAction;
+    public float speed = 3.0f;
+    Rigidbody2D  rigidbody2d;
+    Vector2 Move;
+    
+    public int maxHealth = 5;
+    int currentHealth;
+
     // Start is called before the first frame update
     void Start()
     {
 
         MoveAction.Enable();
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
     }
 
 
-   // Update is called once per frame
-   void Update()
-   {
+    // Update is called once per frame
+    void Update()
+    {
         Vector2 move = MoveAction.ReadValue<Vector2>();
         Debug.Log(move);
-    
+
         Vector2 position = (Vector2)transform.position + move * 0.1f;
         transform.position = position;
-   }
+    }
+    void FixedUpdate()
+    {
+        Vector2 position = (Vector2)rigidbody2d.position + move * speed * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
+    }
+
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+
+    }
+
 }
